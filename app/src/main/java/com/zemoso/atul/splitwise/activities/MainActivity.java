c79fc9b155d1ef3159b175c77cef929571101082
Atul Anand <atul.anand@zemosolabs.com>
2017-08-07 13:19:22 +0530

    Navigation Drawer Debugging

--- app/src/main/java/com/zemoso/atul/splitwise/activities/MainActivity.java
+++ app/src/main/java/com/zemoso/atul/splitwise/activities/MainActivity.java
@@ -1,17 +1,30 @@
 package com.zemoso.atul.splitwise.activities;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.TabLayout;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.zemoso.atul.splitwise.R;
@@ -35,11 +48,22 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     //endregion
 
-    //region Total Balance
-
-    //endregion
 
     private FloatingActionButton fab;
+
+
+    //region Navigation Drawer
+    private LayoutInflater mLayoutInflater;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    private ArrayAdapter mArrayAdapter;
+    private String[] mNavBarTitles;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +88,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Navigation Bar
+        mNavBarTitles = getResources().getStringArray(R.array.nav_bar);
+        mTitle = mDrawerTitle = getSupportActionBar().getTitle();
+
+        mLayoutInflater = getLayoutInflater();
+        mDrawerLayout = (DrawerLayout) mLayoutInflater.inflate(R.layout.nav_bar_drawer,null);
+        mDrawerList = mDrawerLayout.findViewById(R.id.nav_bar_menu);
+
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        mArrayAdapter = new ArrayAdapter<String>(this, R.layout.nav_bar_list_item,mNavBarTitles);
+
+        mDrawerList.setAdapter(mArrayAdapter);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+//                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close){
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
     @Override
     protected void onResume() {
@@ -89,14 +152,85 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        int id = item.getItemId();
-
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action buttons
+        switch(item.getItemId()) {
+            case R.id.action_websearch:
+                // create intent to perform web search for this planet
+//                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+//                intent.putExtra(SearchManager.QUERY, getSupportActionBar().getTitle());
+//                // catch event that there's no activity to handle intent
+//                if (intent.resolveActivity(getPackageManager()) != null) {
+//                    startActivity(intent);
+//                } else {
+                    Toast.makeText(this, "Search not implemented yet.", Toast.LENGTH_LONG).show();
+//                }
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this,"Action Settings",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+//        int id = item.getItemId();
+//
+//        return id == R.id.action_settings || super.onOptionsItemSelected(item) || mDrawerToggle.onOptionsItemSelected(item);
 
     }
 
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
     //endregion
 
+//    /* The click listener for ListView in the navigation drawer */
+//    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+//        @Override
+//        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            selectItem(position);
+//        }
+//    }
+//
+//    private void selectItem(int position) {
+////        // update the main content by replacing fragments
+////        Fragment fragment = new PlanetFragment();
+////        Bundle args = new Bundle();
+////        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
+////        fragment.setArguments(args);
+////
+////        FragmentManager fragmentManager = getFragmentManager();
+////        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+//
+//        // update selected item and title, then close the drawer
+//        mDrawerList.setItemChecked(position, true);
+////        setTitle(mPlanetTitles[position]);
+//        mDrawerLayout.closeDrawer(mDrawerList);
+//    }
+//
+////    @Override
+////    public void setTitle(CharSequence title) {
+////        mTitle = title;
+////        getActionBar().setTitle(mTitle);
+////    }
     //region Private Methods
 
     private void updateFragments(){
